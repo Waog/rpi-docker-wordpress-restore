915278a959e5d4cb133777a100d7113b4ab7c86e
Waog <hello@waog.net>
2016-05-14 19:14:15 +0200

    FIX extracting tars directly into target directory

--- restore.sh
+++ restore.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 echo "restore /app"
 rm -R /app/*
-tar -xpzf /backup/app.tar.gz -C /tmp/
-mv /tmp/app/* /app/
-rm -R tmp/app/
+tar -xpzf /backup/app.tar.gz -C /
 
 echo "restore etc/mysql"
 rm -R /etc/mysql/*
-tar -xpzf /backup/etc-mysql.tar.gz -C /tmp/
-mv /tmp/etc/mysql/* /etc/mysql/
-rm -R tmp/etc
+tar -xpzf /backup/etc-mysql.tar.gz -C /
 
 echo "restore var/lib/mysql"
 rm -R /var/lib/mysql/*
-tar -xpzf /backup/var-lib-mysql.tar.gz -C /tmp/
-mv /tmp/var/lib/mysql/* /var/lib/mysql/
-rm -R tmp/var
+tar -xpzf /backup/var-lib-mysql.tar.gz -C /
 
